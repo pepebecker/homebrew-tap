a6ac730e460d39855da084a9a49a093798ea1a11
Pepe Becker <mail@pepebecker.com>
2023-01-03 07:32:00 -0500

    Update text-recognizer to version 0.2.0

--- text-recognizer.rb
+++ text-recognizer.rb
@@ -1,9 +1,9 @@
 class TextRecognizer < Formula
   desc "TextRecognizer is a command-line interface for performing text recognition on images."
   homepage "https://github.com/pepebecker/osx-text-recognizer"
-  url "https://github.com/pepebecker/osx-text-recognizer/archive/0.1.1.tar.gz"
-  version "0.1.1"
-  sha256 "c807b81e860b130acbb36436d593460040099546ff942ae91c713a391160e038"
+  url "https://github.com/pepebecker/osx-text-recognizer/archive/0.2.0.tar.gz"
+  version "0.2.0"
+  sha256 "652ceac9694d1323755cfb2212ed3eb7d7c642e59fb8372ecd44de7752b1e0d0"
 
   def install
     bin.install "bin/recognize-text"
