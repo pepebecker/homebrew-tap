44fad5726d2d6eaadde79045bd70601c1421c4de
Pepe Becker <mail@pepebecker.com>
2023-01-03 16:36:06 +0900

    Update text-recognizer
    Signed-off-by: Pepe Becker <mail@pepebecker.com>

--- text-recognizer.rb
+++ text-recognizer.rb
@@ -1,9 +1,9 @@
 class TextRecognizer < Formula
-  desc "A command-line interface for performing text recognition on images using Apple's Live Text feature"
+  desc "TextRecognizer is a command-line interface for performing text recognition on images."
   homepage "https://github.com/pepebecker/osx-text-recognizer"
-  url "https://github.com/pepebecker/osx-text-recognizer/archive/0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "a8f661c03a5afa1f40c8f9bde2eb78ed45b7e53e7d5e32e63be385fc43de6744"
+  url "https://github.com/pepebecker/osx-text-recognizer/archive/0.1.1.tar.gz"
+  version "0.1.1"
+  sha256 "c807b81e860b130acbb36436d593460040099546ff942ae91c713a391160e038"
 
   def install
     bin.install "bin/recognize-text"
